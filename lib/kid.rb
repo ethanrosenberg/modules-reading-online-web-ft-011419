1e34a4ffb513f38a5109af77b87ec492ebc8d5fe
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-04 11:18:47 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,10 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
   include Dance
   extend MetaDancing
  
